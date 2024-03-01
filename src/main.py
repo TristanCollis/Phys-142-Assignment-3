ef8b9c63dee6e20e3a9fe684fd3118a4ec91dff5
Tristan Collis <tristan.collis@gmail.com>
2024-02-29 17:58:56 -0800

    Added print statement display results of part 1A

--- src/main.py
+++ src/main.py
@@ -6,9 +6,16 @@ import problem_1c
 import problem_2a
 import problem_2b
 
-# Es, psis = problem_1a.run(save=True)
+Es, psis = problem_1a.run(show=True)
+print(f"{Es[0]=}")
+print(f"{Es[1]=}")
+
+dE = Es[1] - Es[0]
+t_tunnel = np.pi / dE
+print(f"{dE=}")
+print(f"{t_tunnel=}")
 # psi_pdf = problem_1b.run(Es, psis)
 # problem_1c.run(Es, psis)
 
 # problem_2a.run(N=20, temperature=2, steps=200_000)
-problem_2b.run(N=20, temperatures=np.arange(1, 4, 0.2), burn_in=100_000, steps=200_000, save=True)
+# problem_2b.run(N=20, temperatures=np.arange(1, 4, 0.2), burn_in=100_000, steps=200_000, save=True)
